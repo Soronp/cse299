166c144199900aa7395b54dd8697454617d6457e
Fahim Hasan Pradhan <fahim.pradhan@northsouth.edu>
2024-03-11 05:59:21 +0600

    asdsad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
   return (
@@ -7,16 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CSE299
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />;
+
       </header>
     </div>
   );
