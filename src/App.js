cf99def9ac915bcec5a21785f20b730b13750705
Fahim Hasan Pradhan <fahim.pradhan@northsouth.edu>
2024-03-11 06:05:23 +0600

    new assessments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showCreateAssessmentButton, setShowCreateAssessmentButton] = useState(false);
+
+  const handleLoginSuccess = credentialResponse => {
+    console.log(credentialResponse);
+    setIsLoggedIn(true);
+    setShowCreateAssessmentButton(true); // Show create assessment button after login
+  };
+
+  const handleLoginError = () => {
+    console.log('Login Failed');
+  };
+
+  const handleCreateAssessmentClick = () => {
+    // Logic to handle create assessment button click
+    console.log('Create assessment button clicked');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +29,15 @@ function App() {
         <p>
           CSE299
         </p>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />;
-
+        {!isLoggedIn && (
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={handleLoginError}
+          />
+        )}
+        {isLoggedIn && showCreateAssessmentButton && (
+          <button onClick={handleCreateAssessmentClick}>Create New Assessment</button>
+        )}
       </header>
     </div>
   );
