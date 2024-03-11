5641254ace95b65506f1954013f7b952721c21ea
Fahim Hasan Pradhan <fahim.pradhan@northsouth.edu>
2024-03-11 07:36:43 +0600

    Google Forms and Sheets done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { GoogleLogin } from '@react-oauth/google';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showCreateAssessmentButton, setShowCreateAssessmentButton] = useState(false);
+  const [googleSheetsLink, setGoogleSheetsLink] = useState('');
+  const [googleFormsLink, setGoogleFormsLink] = useState('');
 
   const handleLoginSuccess = credentialResponse => {
     console.log(credentialResponse);
@@ -20,15 +22,28 @@ function App() {
   const handleCreateAssessmentClick = () => {
     // Logic to handle create assessment button click
     console.log('Create assessment button clicked');
+    // Prompt user to input Google Sheets and Google Forms links
+    const sheetsLink = prompt('Enter Google Sheets link:');
+    setGoogleSheetsLink(sheetsLink);
+    const formsLink = prompt('Enter Google Forms link:');
+    setGoogleFormsLink(formsLink);
+  };
+
+  const handleSheetsSubmit = () => {
+    // Open Google Sheets link
+    window.open(googleSheetsLink, '_blank');
+  };
+
+  const handleFormsSubmit = () => {
+    // Open Google Forms link
+    window.open(googleFormsLink, '_blank');
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CSE299
-        </p>
+        <p>CSE299</p>
         {!isLoggedIn && (
           <GoogleLogin
             onSuccess={handleLoginSuccess}
@@ -36,7 +51,15 @@ function App() {
           />
         )}
         {isLoggedIn && showCreateAssessmentButton && (
-          <button onClick={handleCreateAssessmentClick}>Create New Assessment</button>
+          <div>
+            <button onClick={handleCreateAssessmentClick}>Create New Assessment</button>
+            {googleSheetsLink && (
+              <button onClick={handleSheetsSubmit}>Submit Google Sheets</button>
+            )}
+            {googleFormsLink && (
+              <button onClick={handleFormsSubmit}>Submit Google Forms</button>
+            )}
+          </div>
         )}
       </header>
     </div>
